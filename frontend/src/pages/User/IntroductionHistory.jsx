680d68ab56f0ced21182d492bb5082137183a7cc
mshahilt <mshahilt3@gmail.com>
2025-02-05 22:39:48 +0530

    Introduction to history

---
+++ frontend/src/pages/User/IntroductionHistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IntroductionHistory = () => {
+  return (
+    <div>
+      IntroductionHistory to history component. page
+    </div>
+  )
+}
+
+export default IntroductionHistory
